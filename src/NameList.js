eb9a8ac09b20f52168fc41cffd4f20c7ec4bcb88
mikhita <mikheilispiriani@yahoo.com>
2022-09-27 15:02:06 +0400

    I learn when I can use index and in which cases it will be a hindrance to my code

--- src/NameList.js
+++ src/NameList.js
@@ -1,36 +1,37 @@
 import React from 'react'
-import Person from './Person';
+// import Person from './Person';
 
 function PersonList() {
-    const people = [
-        {
-            id:1,
-            name:"nika",
-            age:30,
-            skill:"Yvelaferchik"
-        },
-        {
-            id:2,
-            name:"Giorgi",
-            age:19,
-            skill:"react"
-        },
-        {
-            id:3,
-            name:"Beqa",
-            age:22,
-            skill:"AngulAr"
-        },
-        {
-            id:4,
-            name:"Badu",
-            age:25,
-            skill:"js"
-        }
-    ]
-    const personList = people.map(person=><Person person={person}/>);
+    const names = ["nika", "gio", "beqa"]
+    // const people = [
+    //     {
+    //         id:1,
+    //         name:"nika",
+    //         age:30,
+    //         skill:"Yvelaferchik"
+    //     },
+    //     {
+    //         id:2,
+    //         name:"Giorgi",
+    //         age:19,
+    //         skill:"react"
+    //     },
+    //     {
+    //         id:3,
+    //         name:"Beqa",
+    //         age:22,
+    //         skill:"AngulAr"
+    //     },
+    //     {
+    //         id:4,
+    //         name:"Badu",
+    //         age:25,
+    //         skill:"js"
+    //     }
+    // ]
+    const nameList = names.map((name, index)=><h2 key={index}> {name}</h2>);
   return (
-        <div>{personList}</div>
+        <div>{nameList}</div>
     )
 }
 
