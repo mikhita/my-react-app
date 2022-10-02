ccd66074cdcccc0bdc0b73e0f3e15df9a678ca44
mikhita <mikheilispiriani@yahoo.com>
2022-10-02 17:07:54 +0400

    I learn how to use second parameter of useEfffect function to change it with only changing the target object

--- src/components/CounterTwo.js
+++ src/components/CounterTwo.js
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from 'react'
 
 function CounterTwo() {
     const [ count  ,  setCount ] = useState(0);
+    const [name, setName] = useState("")
 
     useEffect(()=>{
-      document.title=`Clicked ${count} `
-    })
+      console.log("eseeffect")
+    }, [count])
   return (
     <div>
+        <input type="text" value={name} onChange={e=>setName(e.target.value)}/>
         <button onClick={()=>(setCount(count+1))}>Increment:{count}</button>
     </div>
   )
